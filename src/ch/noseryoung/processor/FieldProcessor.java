3f436e1d75d91a875fc6ac94b82c59fef4af4e81
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2022-01-17 20:32:20 +0100

    adding FieldProcessor

---
+++ src/ch/noseryoung/processor/FieldProcessor.java
@@ -0,0 +1,10 @@
+package ch.noseryoung.processor;
+
+import ch.noseryoung.Organism;
+
+public class FieldProcessor {
+    private final static int Y_AXIS_SIZE = 100;
+    private final static int X_AXIS_SIZE = 100;
+    private final static Organism[][] field = new Organism[Y_AXIS_SIZE][X_AXIS_SIZE];
+
+}
