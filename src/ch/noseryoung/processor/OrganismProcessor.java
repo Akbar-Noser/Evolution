a4e1a5415d90525bfe2b1e7e28cc8640b8ca0c3e
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2022-01-17 20:02:56 +0100

    adding OrganismProcessor

---
+++ src/ch/noseryoung/processor/OrganismProcessor.java
@@ -0,0 +1,25 @@
+package ch.noseryoung.processor;
+
+import ch.noseryoung.Organism;
+
+import java.util.ArrayList;
+
+public class OrganismProcessor {
+    private ArrayList<Organism> organisms;
+
+    public OrganismProcessor() {
+        this.organisms = new ArrayList<>();
+    }
+
+    public void generateInitialOrganisms(int amount) {
+        for (int i = 0; i < amount; i++) {
+            organisms.add(new Organism());
+        }
+    }
+
+ 
+
+    public ArrayList<Organism> getOrganisms() {
+        return organisms;
+    }
+}
